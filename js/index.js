147a8e243ba2ae0f90f17fca29238c07c29049bd
ChristiaChilatra <chilatra1998@gmail.com>
2021-11-19 01:15:37 -0500

    Sin commentarios

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import { getPokemonId, getPokemonSpecies} from "./service/data.js"
-import showPokemon from "./show-pokemon.js"
+import {showPokemon} from "./show-pokemon.js"
 import showSpeciesPokemon from "./show-species-pokemon.js"
 
 
@@ -8,6 +8,7 @@ const $formulario = document.querySelector("#form")
 getPokemon()
 
 function  getPokemon() {
+
   $formulario.addEventListener("submit", async (event) =>{
     event.preventDefault()
     const form = new FormData($formulario)
@@ -25,6 +26,6 @@ function  getPokemon() {
       console.error(error);
     }
 
-
   })
+
 }
--- js/show-pokemon.js
+++ js/show-pokemon.js
@@ -2,8 +2,10 @@ const $containerPokemon = document.querySelector(".showPokemon")
 const $loader = document.querySelector(".loader")
 const $message = document.querySelector(".message")
 
-export default function showPokemon(response) {
 
+
+export function showPokemon(response) {
+  
   let counter = 0
 
   const sprite = response.sprites
@@ -14,13 +16,46 @@ export default function showPokemon(response) {
     }
   }
 
-  const spriteKey = Object.keys(sprite).reverse()
+  if (counter > Object.values(sprite).length - 1) {
+    counter = Object.values(sprite).length - 1
+  }
+  if (counter < 0) {
+    counter = 0
+  }
 
-  console.log(spriteKey);
+  const spriteKey = Object.keys(sprite).reverse()
 
   $message.innerHTML = ""
   $loader.style.display = "none"
 
+  setImagePokemon(sprite, spriteKey, counter)
+  setAddEventListener()
+
+  function setAddEventListener() {
+    const $navegation = document.querySelector(".pokemonSwitch")
+    const $buttonUp = $navegation.querySelector(".button.up")
+    const $buttonDown = $navegation.querySelector(".button.down")
+
+    $buttonUp.removeEventListener("click", nextPokemon)
+    $buttonDown.removeEventListener("click", prevPokemon)
+    $buttonUp.addEventListener("click", nextPokemon)
+    $buttonDown.addEventListener("click", prevPokemon)
+  }
+
+
+  function nextPokemon() {
+    console.log(counter);
+    counter++
+    setImagePokemon(sprite, spriteKey, counter)
+  }
+  function prevPokemon() {
+    console.log(counter);
+    counter--
+    setImagePokemon(sprite, spriteKey, counter)
+  }
+}
+
+function setImagePokemon(sprite, spriteKey, counter) {
   $containerPokemon.querySelector("img").setAttribute("src", ``)
   $containerPokemon.querySelector("img").setAttribute("src", `${sprite[spriteKey[counter]]}`)
 }
